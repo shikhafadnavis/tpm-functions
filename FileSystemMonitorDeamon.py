8ee8d08abf7205069f3b9a7d3710cd92f840d8be
Shikha Fadnavis <shikhafadnavis@gmail.com>
2017-12-09 17:02:00 -0500

    Adding routines for handling file changes

--- FileSystemMonitorDeamon.py
+++ FileSystemMonitorDeamon.py
@@ -1,7 +1,11 @@
-import time
+import time, os, os.path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import tpm-script as tpmFunctions
 
+#################################
+
+masterBookKeeper = []
 
 class Watcher:
     DIRECTORY_TO_WATCH = "/home/sfubuntu"
@@ -32,13 +36,44 @@ class Handler(FileSystemEventHandler):
 
         elif event.event_type == 'created':
             # Take any action here when a file is first created.
-            print "Received created event - %s." % event.src_path
+            # True indicates that the file is yet to be processed by the Encryption Engine
+            masterBookKeeper.append([event.src_path,True])
 
-        elif event.event_type == 'modified':
-            # Taken any action here when a file is modified.
-            print "Received modified event - %s." % event.src_path
+def encryptFile(filename, emailID):
+        #get public key for the user
+        tpmFunctions.encryptData(filename, emailID)
+
+def _getCurrentTime()
+	return time.now()
+
+def masterHandler()
+
+	emailID1 = "sfad@fg.com"
+        emailID2 = "vgop@fg.com"
+        for i in range(0,len(masterBookKeeper)):
+		if masterBookKeeper[i][1] == True:
+			# Encrypting the file
+			print "%s is being encrypted" %masterBookKeeper[i][0]
+			timeBefore = _getCurrentTime()
+			encryptFile(masterBookKeeper[i][0],emailID1)
+			timeAfter = _getCurrentTime()
+			deltaEncryption = timeAFter - timeBefore
+			# Post encryption operation unset the modifier flag
+			masterBookKeeper[i][1] = False
+
+		else:
+			print "%s is already enrypted" % masterBookKeeper[i][0]
+
+def main():
+	# Start the asycio event handler
+	w = watcher()
+	w.run()
+	# Master Handler monitors for file changes
+	masterHandler()
+
+if __name__ == "__main__":
+        main()
 
 
-if __name__ == '__main__':
-    w = Watcher()
-    w.run()
+
+
