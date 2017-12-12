ac4a80727f6e8903e4c6bdfa681162bb0395b52b
Shikha <shikhafadnavis@gmail.com>
2017-12-11 22:09:06 -0800

    changes to plot graph

--- plot_graph.py
+++ plot_graph.py
@@ -2,21 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os, sys
 
-numFiles = [1, 10, 100, 500, 1000, 2000, 4000]
-avgTime = [0.002681016922, 0.00439212322235, 0.00621670007706, 0.00709121370316, 0.00695849990845, 0.00781035757065, 0.00731262212992]
+numFiles = [1, 500, 1000, 2000, 4000]
+numFilesbar2 = map(lambda x:x+200, numFiles)
+avgTime = [0.002681016922, 0.00709121370316, 0.00695849990845, 0.00781035757065, 0.00731262212992]
+avgTime2 = [0.0134689807892, 0.0123686718941, 0.0125782625675,  0.0122886763811, 0.0136354961991]
 
 def graphPlot(typeGraph):
-	y_pos = range(len(numFiles))
-	bar_width = 0.5
+
 	if typeGraph == "line":
 		plt.plot(numFiles, avgTime)
+		#plt.plot(numFiles, avgTime2)
 	elif typeGraph == "bar":
-		plt.bar(numFiles, avgTime, width=200, align='center', alpha=0.3)
-	#plt.xticks(y_pos, numFiles)
+		plt.bar(numFiles, avgTime, width=200, color='b', align='center', alpha=0.3)
+		plt.bar(numFilesbar2, avgTime2, width=200, color='r', align='center', alpha=0.3)
+	plt.xticks(np.arange(min(numFiles), max(numFiles)+200, 500.0))
 	plt.xlabel('Number of Files encrypted')
 	plt.ylabel('Average Time')
 	plt.title('Encryption time plot')
-	plt.axis([0, max(numFiles), 0, max(avgTime)])
+	plt.axis([0, max(numFiles)+500, 0, max(avgTime)+0.01])
 	plt.show()
 
 def main():
