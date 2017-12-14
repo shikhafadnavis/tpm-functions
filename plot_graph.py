3317be30fa0eb22fb18ee091e80d5d3f427d4808
Shikha <shikhafadnavis@gmail.com>
2017-12-14 14:46:39 -0800

    changes to line graph: include std dev

--- plot_graph.py
+++ plot_graph.py
@@ -5,13 +5,18 @@ import os, sys
 numFiles = [1, 500, 1000, 2000, 4000]
 numFilesbar2 = map(lambda x:x+200, numFiles)
 avgTime = [0.002681016922, 0.00709121370316, 0.00695849990845, 0.00781035757065, 0.00731262212992]
-avgTime2 = [0.0134689807892, 0.0123686718941, 0.0125782625675,  0.0122886763811, 0.0136354961991]
+avgTime2 = [0.0119068622589, 0.0124842896461,  0.0136637945175,  0.0123260861635, 0.0123281091452]
+stdDevEnc = [0.00018388492719341947, 3.9082478279393022e-05, 0.00039358891772538332, 0.00028649386533868956, 5.224722831151822e-05]
+stdDevDec = [0, 0.00139621787504, 0.00655269418534, 0.00208470978633, 0.00202869676072]
+
 
 def graphPlot(typeGraph):
 
 	if typeGraph == "line":
-		plt.plot(numFiles, avgTime)
-		#plt.plot(numFiles, avgTime2)
+		plt.plot(numFiles, avgTime, '-b', label='Encryption')
+		plt.plot(numFiles, avgTime2, 'r', label='Decryption')
+		plt.plot(numFiles, stdDevEnc, 'g', label='Standard Deviation Encryption')
+		plt.plot(numFiles, stdDevDec, label='Standard Deviation Decryption')
 	elif typeGraph == "bar":
 		plt.bar(numFiles, avgTime, width=200, color='b', align='center', alpha=0.3)
 		plt.bar(numFilesbar2, avgTime2, width=200, color='r', align='center', alpha=0.3)
